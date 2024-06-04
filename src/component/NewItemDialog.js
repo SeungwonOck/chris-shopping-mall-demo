13bce82f334f077001e1e602234d43aad82f93fb
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-06-05 00:45:19 +1000

    Create Product - Upload Image

--- src/component/NewItemDialog.js
+++ src/component/NewItemDialog.js
@@ -25,9 +25,9 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
     mode === "new" ? { ...InitialFormData } : selectedProduct
   );
   const [stock, setStock] = useState([]);
+  console.log("rr", stock)
   const dispatch = useDispatch();
   const [stockError, setStockError] = useState(false);
-  console.log("stock", stock)
   const handleClose = () => {
     //모든걸 초기화시키고;
     // 다이얼로그 닫아주기
@@ -36,10 +36,16 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     //재고를 입력했는지 확인, 아니면 에러
+    if (stock.length === 0) setStockError(true);
     // 재고를 배열에서 객체로 바꿔주기
     // [['M',2]] 에서 {M:2}로
+    const totalStock = stock.reduce((total, item) => {
+      return {...total, [item[0]]: parseInt(item[1])}
+    }, {})
     if (mode === "new") {
       //새 상품 만들기
+      dispatch(productActions.createProduct({ ...formData, stock: totalStock }))
+      setShowDialog(false);
     } else {
       // 상품 수정하기
     }
@@ -95,6 +101,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 
   const uploadImage = (url) => {
     //이미지 업로드
+    setFormData({...formData, image: url})
   };
 
   useEffect(() => {
