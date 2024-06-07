961fb449a6ffadd231be0c29707e2270a2c832d1
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-06-08 02:39:49 +1000

    Showing products in landing page & Add Search Function

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -8,15 +8,36 @@ import { commonUiActions } from "../action/commonUiAction";
 
 const ProductAll = () => {
   const dispatch = useDispatch();
-  const error = useSelector((state) => state.product.error);
+  const { productList } = useSelector((state) => state.product);
+  const [query, setQUery] = useSearchParams();
+  const name = query.get("name");
   // 처음 로딩하면 상품리스트 불러오기
+  useEffect(() => {
+    dispatch(
+      productActions.getProductList({
+        name,
+      })
+    );
+  }, [query]);
 
   return (
     <Container>
       <Row>
-        <Col md={3} sm={12}>
-          <ProductCard />
-        </Col>
+        {productList && productList.length > 0 ? (
+          productList.map((item) => (
+            <Col md={3} sm={12} key={item._id}>
+              <ProductCard item={item} />
+            </Col>
+          ))
+        ) : (
+          <div>
+            {name === "" ? (
+              <h2>There are no registered products</h2>
+            ) : (
+              <h2>No product matches with {name}</h2>
+            )}
+          </div>
+        )}
       </Row>
     </Container>
   );
