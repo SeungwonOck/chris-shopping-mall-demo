70e43e8af883572025cae6d1cddb8a9b56292440
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-06-08 01:53:53 +1000

    Product Edit & Delete Function

--- src/action/productAction.js
+++ src/action/productAction.js
@@ -31,9 +31,34 @@ const createProduct = (formData) => async (dispatch) => {
     dispatch(commonUiActions.showToastMessage(error.error, "error"))
   }
 };
-const deleteProduct = (id) => async (dispatch) => {};
+const deleteProduct = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: types.PRODUCT_DELETE_REQUEST });
+    const response = await api.delete(`/product/${id}`);
+    if (response.status !== 200) throw new Error(response.error);
+    dispatch({ type: types.PRODUCT_DELETE_SUCCESS });
+    dispatch(commonUiActions.showToastMessage("Product deleted!", "success"));
 
-const editProduct = (formData, id) => async (dispatch) => {};
+    dispatch(getProductList({ page: 1, name: "" }));
+  } catch (error) {
+    dispatch({ type: types.PRODUCT_DELETE_FAIL, payload: error.error });
+    dispatch(commonUiActions.showToastMessage(error.error, "error"));
+  }
+};
+
+const editProduct = (formData, id) => async (dispatch) => {
+  try {
+    dispatch({ type: types.PRODUCT_EDIT_REQUEST })
+    const response = await api.put(`/product/${id}`, formData)
+    if (response.status !== 200) throw new Error(response.error)
+    dispatch({ type: types.PRODUCT_EDIT_SUCCESS, payload: response.data.data })
+    dispatch(commonUiActions.showToastMessage("Product Edited!", "success"))
+    dispatch(getProductList({page: 1, name:""}))
+  } catch (error) {
+    dispatch({ type: types.PRODUCT_EDIT_FAIL, payload: error.error })
+    dispatch(commonUiActions.showToastMessage(error.error, "error"))
+  }
+};
 
 export const productActions = {
   getProductList,
